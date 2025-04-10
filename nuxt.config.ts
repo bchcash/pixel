c5045823676fe55a113a5c3b5964dc5610d769aa
[ G'Lab ] Team <92644489+bchcash@users.noreply.github.com>
2025-04-10 23:54:21 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,20 @@
-import { defineNuxtConfig } from 'nuxt'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // Модули
   modules: [
     '@nuxthub/core',
     '@nuxt/ui',
     '@nuxt/eslint',
     'nuxt-auth-utils',
   ],
-
-  // Включаем инструменты разработчика
   devtools: { enabled: true },
-
-  // Стили CSS
   css: ['~/assets/main.css'],
-
-  // Настройки совместимости с более старыми версиями
   future: { compatibilityVersion: 4 },
-
-  // Дата совместимости для Nuxt Nitro
   compatibilityDate: '2024-07-30',
-
-  // Настройки для Hub
   hub: {
     ai: false,
     blob: true,
   },
-
-  // Конфигурация для eslint
+  // Development modules
   eslint: {
     config: {
       stylistic: {
@@ -35,15 +22,11 @@ export default defineNuxtConfig({
       },
     },
   },
-
-  // Настройки для Cloudflare Pages
-  nitro: {
+   // Настройки для Cloudflare Pages
+   nitro: {
     preset: 'cloudflare-pages', // Это указывает, что проект будет развернут на Cloudflare Pages
     output: {
       dir: '.output/cloudflare', // Указывает на папку, в которую будет собираться проект для Cloudflare
     },
-  },
-
-  // Включение режима разработки Cloudflare
-  buildModules: ['@nuxtjs/cloudflare-workers'], // Если вы используете cloudflare-workers для деплоя (по желанию)
+  }
 })
